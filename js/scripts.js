5f3a1e775890c3360c1b94837ba82083da607211
Una Kravets <Una21136@yahoo.com>
2013-10-06 15:43:13 -0400

    panels working wiht jquery mobile

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
-//**** IF YOU'RE ON THE RIGHT PAGE *****//
-$(function(){
-  // Bind the swipeleftHandler callback function to the swipe event on div.box
-  $('.individual-entry').on('swipeleft', openEntryNav );
-  $('.individual-entry, .entry-nav').on('swiperight', closeEntryNav );
-  $('.entry-nav-btn').on('click', toggleEntryNav)
+// //**** IF YOU'RE ON THE RIGHT PAGE *****//
+// $(function(){
+//   // Bind the swipeleftHandler callback function to the swipe event on div.box
+//   $('.individual-entry').on('swipeleft', openEntryNav );
+//   $('.individual-entry, .entry-nav').on('swiperight', closeEntryNav );
+//   $('.entry-nav-btn').on('click', toggleEntryNav)
  
-  // Callback function references the event target and adds the 'swipeleft' class to it
-  function openEntryNav(){
-    $('.entry-nav').addClass('pulled');
-  }  
-  function closeEntryNav(){
-    $('.entry-nav').removeClass('pulled');
-  }  
-  function toggleEntryNav(){
-    $('.entry-nav').toggleClass('pulled');
-  }
-});
+//   // Callback function references the event target and adds the 'swipeleft' class to it
+//   function openEntryNav(){
+//     $('.entry-nav').addClass('pulled');
+//   }  
+//   function closeEntryNav(){
+//     $('.entry-nav').removeClass('pulled');
+//   }  
+//   function toggleEntryNav(){
+//     $('.entry-nav').toggleClass('pulled');
+//   }
+// });
