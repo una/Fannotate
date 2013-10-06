94bd6c89e614eb7039347d158fe735d97ca635fe
Una Kravets <Una21136@yahoo.com>
2013-10-06 17:46:09 -0400

    sorting works and turns on and off

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,24 @@
-// //**** IF YOU'RE ON THE RIGHT PAGE *****//
-// $(function(){
-//   // Bind the swipeleftHandler callback function to the swipe event on div.box
-//   $('.individual-entry').on('swipeleft', openEntryNav );
-//   $('.individual-entry, .entry-nav').on('swiperight', closeEntryNav );
-//   $('.entry-nav-btn').on('click', toggleEntryNav)
- 
-//   // Callback function references the event target and adds the 'swipeleft' class to it
-//   function openEntryNav(){
-//     $('.entry-nav').addClass('pulled');
-//   }  
-//   function closeEntryNav(){
-//     $('.entry-nav').removeClass('pulled');
-//   }  
-//   function toggleEntryNav(){
-//     $('.entry-nav').toggleClass('pulled');
-//   }
-// });
+$(function(){
+	$(".all-thumbs").sortable().sortable('disable');
+	$('.sortToggle').on('click', allowSorting );
+	var onSort = false;
+
+	function allowSorting() {
+
+		if (onSort) {
+			$(".all-thumbs").sortable('disable');
+			$( ".all-thumbs li" ).removeClass('shaking');
+			 console.log('hi');
+			 onSort = false;
+		}
+		else {
+		$(".all-thumbs").sortable('enable');
+		// $( ".all-thumbs" ).disableSelection();
+		$(".all-thumbs li").addClass('shaking');
+		onSort = true;
+	 	}
+	};
+});
+
+
+
