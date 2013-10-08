e346f5549230c98a96dc1023307d851869641b75
Una Kravets <Una21136@yahoo.com>
2013-10-08 14:21:58 -0400

    whatever artist you click, it shows up as the next pages header

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,15 @@
 $(function(){
+
+	// Sorting Page Functions
 	$(".all-thumbs").sortable().sortable('disable');
 	$('.sortToggle').on('click', allowSorting );
+	$('.artist-link').on('click', getArtist );
 	var onSort = false;
 
 	function allowSorting() {
-
 		if (onSort) {
 			$(".all-thumbs").sortable('disable');
 			$( ".all-thumbs li" ).removeClass('shaking');
-			 console.log('hi');
 			 onSort = false;
 		}
 		else {
@@ -18,7 +19,14 @@ $(function(){
 		onSort = true;
 	 	}
 	};
-});
+
+	function getArtist(e) {
+		console.log(e.currentTarget.children[1].innerHTML);
+		var currentArtist = (e.currentTarget.children[1].innerHTML)
+		$('.current-artist').html(currentArtist);
+	}
+
+}); // end of SIAF
 
 
 
